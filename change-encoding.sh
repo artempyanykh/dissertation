b032de881e8c13c602ebab38cf41ed750a65cefb
Artem Pyanykh <artem.pyanykh@gmail.com>
2016-05-13 08:42:06 +0300

    Prepare for encoding conversion

---
+++ change-encoding.sh
@@ -0,0 +1,54 @@
+# This script converts all the source files into CP1251
+
+from=utf8
+to=cp1251
+
+function convert {
+  for file in `find . -name '*.tex' | grep -v $to`; do
+    convname=`echo $file | sed "s/\.tex/\.$to\.tex/"`
+    [ -f $convname ] && rm $convname
+    echo "conv $file --> $convname"
+    iconv -f $from -t $to "$file" > $convname
+  done
+
+  for file in `find . -name '*.bib' | grep -v $to`; do
+    convname=`echo $file | sed "s/\.bib/\.$to\.bib/"`
+    [ -f $convname ] && rm $convname
+    echo "conv $file --> $convname"
+    iconv -f $from -t $to "$file" > $convname
+  done
+}
+
+function move {
+  for file in `find . -name "*.$to.tex" | grep -v git`; do
+    mvname=`echo $file | sed "s/\.$to//"`
+    # echo "mv $file --> $mvname"
+    mv -v "$file" "$mvname"
+  done
+
+  for file in `find . -name "*.$to.bib" | grep -v git`; do
+    mvname=`echo $file | sed "s/\.$to//"`
+    # echo "mv $file --> $mvname"
+    mv -v "$file" "$mvname"
+  done
+}
+
+function remove {
+  for file in `find . -name "*$to*" | grep -v git`; do
+    rm -v "$file"
+  done
+}
+
+case $1 in
+  -c|--convert)
+    convert
+    ;;
+  -m|--move)
+    move
+    ;;
+  -r|--remove)
+    remove
+    ;;
+  *)
+    echo 'You need to provide either -c or -m argument'
+esac
